3446a1b33a3d07040eec4ec558bde36dee2ffc52
pfg <pfg@pfg.pw>
2025-04-22 20:28:54 -0700

    test-assert-typedarray-deepequal.js

---
+++ repro.js
@@ -0,0 +1,1 @@
+require("assert").deepEqual(new SharedArrayBuffer(1), new ArrayBuffer(1));
--- src/js/node/assert.ts
+++ src/js/node/assert.ts
@@ -23,7 +23,17 @@
 
 const { SafeMap, SafeSet, SafeWeakSet } = require("internal/primordials");
 const { Buffer } = require("node:buffer");
-const { isKeyObject, isPromise, isRegExp, isMap, isSet, isDate, isWeakSet, isWeakMap } = require("node:util/types");
+const {
+  isKeyObject,
+  isPromise,
+  isRegExp,
+  isMap,
+  isSet,
+  isDate,
+  isWeakSet,
+  isWeakMap,
+  isAnyArrayBuffer,
+} = require("node:util/types");
 const { innerOk } = require("internal/assert/utils");
 const { validateFunction } = require("internal/validators");
 
@@ -32,6 +42,7 @@ const ArrayPrototypeIndexOf = Array.prototype.indexOf;
 const ArrayPrototypeJoin = Array.prototype.join;
 const ArrayPrototypePush = Array.prototype.push;
 const ArrayPrototypeSlice = Array.prototype.slice;
+const ArrayBufferIsView = ArrayBuffer.isView;
 const NumberIsNaN = Number.isNaN;
 const ObjectAssign = Object.assign;
 const ObjectIs = Object.is;
@@ -380,6 +391,16 @@ const SafeSetPrototypeIterator = SafeSet.prototype[SymbolIterator];
 function compareBranch(actual, expected, comparedObjects?) {
   // Check for Map object equality
   if (isMap(actual) && isMap(expected)) {
+    return Bun.deepEquals(actual, expected, true);
+  }
+
+  // Check for ArrayBuffer object equality
+  if (
+    ArrayBufferIsView(actual) ||
+    isAnyArrayBuffer(actual) ||
+    ArrayBufferIsView(expected) ||
+    isAnyArrayBuffer(expected)
+  ) {
     return Bun.deepEquals(actual, expected, true);
   }
 
